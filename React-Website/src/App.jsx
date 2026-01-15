6bd373e7c5df4edc9c8d97299d9eb76f425bc7ed
Maxine Yang <161282981+yangmma@users.noreply.github.com>
2026-01-15 15:05:25 +1300

    Parallax background added

--- React-Website/src/App.jsx
+++ React-Website/src/App.jsx
@@ -1,5 +1,6 @@
 // gen:
 import React from "react";
+import { ParallaxProvider } from 'react-scroll-parallax';
 import { Routes, Route } from "react-router-dom";
 import Page1 from "./pages/About.jsx";
 import Page2 from "./pages/Experience";
@@ -10,13 +11,13 @@ import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <>
+    <ParallaxProvider>
       <NavBar />
       <Routes>
         <Route path="/" element={<Page1/>} />
         <Route path="/experience" element={<Page2/>} />
       </Routes>
-    </>
+    </ParallaxProvider>
   );
 }
 
--- React-Website/src/pages/About.jsx
+++ React-Website/src/pages/About.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import AboutBox from '../components/AboutBox';
 import styled, { keyframes } from 'styled-components';
+import GreyVolumeA from '../assets/GreyVolume.svg?react';
+import YellowVolumeA from '../assets/YellowVolume.svg?react'
+import { useParallax } from 'react-scroll-parallax';
 
 const Animation = keyframes`
     0% {
@@ -15,14 +18,64 @@ const Animation = keyframes`
 `;
 
 const AnimatedWrapper = styled.div`
-    animation: ${Animation} 1s ease-out forwards;
+    animation: ${Animation} 2s ease-out forwards;
 `
+const VolumeWrapper = styled.div`
+    position: absolute;
+    inset: 0;
+    pointer-events: none;
+    z-index: -1;
+`;
 
+const VolumeIcon1 = styled(GreyVolumeA)`
+  width: 75vw;
+  height: 75vh;
+  position: absolute;
+  top: 110vh;
+  left: -10vw;
+  z-index: -1;
+  rotate: -27.5deg;
+`;
+
+const VolumeIcon2 = styled(YellowVolumeA)`
+  width: 420vw;
+  height: 250vh;
+  top: -60vh;
+  position: absolute;
+  right: -160vw;
+  z-index: -1;
+  rotate: -10deg;
+`;
+
+const BackgroundWrapper = styled.div`
+    position: relative;
+`;
 
 export default function Page1() {
+    const parallax = useParallax({
+        speed: -15,
+    });
+
+    const parallax2 = useParallax({
+        speed: -20,
+    });
+
     return (
         <AnimatedWrapper>
+            <BackgroundWrapper>
+                <div>
                     <AboutBox />
+                </div>
+                    <VolumeWrapper ref={parallax.ref}>
+                        <VolumeIcon1 />
+                    </VolumeWrapper>
+                    <VolumeWrapper ref={parallax2.ref}>
+                        <VolumeIcon2/>
+                    </VolumeWrapper>
+                    
+            </BackgroundWrapper>
+            <div style={{ height: '200vh' }} />
+
         </AnimatedWrapper>
     );
 }

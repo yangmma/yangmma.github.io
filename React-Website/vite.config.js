c02e51b9507a51c292433ca8dd1b76862113711d
Maxine Yang <161282981+yangmma@users.noreply.github.com>
2026-03-05 15:09:12 +1300

    Build site to docs

--- React-Website/vite.config.js
+++ React-Website/vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [react(), svgr()],
-})
+  build: {
+    outDir: '../docs'
+  }
+});

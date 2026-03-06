44e630c77d444383a3e1dda490040eec467f5c97
Maxine Yang <161282981+yangmma@users.noreply.github.com>
2026-03-06 21:09:03 +1300

    Changed to HashRouter

--- React-Website/src/main.jsx
+++ React-Website/src/main.jsx
@@ -4,12 +4,12 @@ import '@fontsource/inter/700.css';
 
 import React from "react";
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
 )

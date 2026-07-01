ba420483739fb9e6353d41e656ddcdfa195673a9
Maxine Yang <161282981+yangmma@users.noreply.github.com>
2026-07-01 12:01:00 +1200

    Email layout fix

--- React-Website/src/components/widgets/Footer.jsx
+++ React-Website/src/components/widgets/Footer.jsx
@@ -13,7 +13,7 @@ const Bolded = styled.span`
 
 export default function Footer () {
     return(<>
-        <FooterContainer> <a style={{ textDecoration: "underline" }} href="mailto:yangmma03@gmail.com">yangmma03 [at] gmail [dot] com </a> <Bolded>|</Bolded> <a style={{ textDecoration: "underline" }} href="https://www.linkedin.com/in/mma-yang/" target="_blank"> /in/mma-yang/ </a> </FooterContainer>
+        <FooterContainer> <a style={{ textDecoration: "underline" }} href="mailto:yangmma03@gmail.com">yangmma03 [at] gmail [dot] com</a> <Bolded>|</Bolded> <a style={{ textDecoration: "underline" }} href="https://www.linkedin.com/in/mma-yang/" target="_blank"> /in/mma-yang/ </a> </FooterContainer>
     </>)
 
 }
